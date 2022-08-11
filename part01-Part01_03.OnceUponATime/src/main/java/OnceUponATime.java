742b90bc19b3608ce8da510549491ec9b97f59e0
Dominykas Genys <dominykasgenysmail@gmail.com>
2022-08-11 14:46:55 +0300

    Use three System.out.println-commands for printing.

--- part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
+++ part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
@@ -3,5 +3,11 @@ public class OnceUponATime {
 
     public static void main(String[] args) {
         // Write your program here
+//        Modify the program so that it will print the following text. 
+//        Use three System.out.println-commands for printing.
+
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
     }
 }
